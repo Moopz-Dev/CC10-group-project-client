1ebbaae3ae9e368a61e342a80dd699e666e086df
Moopz-Dev <pongzthor@gmail.com>
2022-02-10 15:32:44 +0700

    fix component behaviour, correct server hostname in env file

--- src/components/auth/FacebookLoginButton.jsx
+++ src/components/auth/FacebookLoginButton.jsx
@@ -2,27 +2,29 @@ import React from "react";
 import FacebookLogin from "react-facebook-login";
 import { facebookLogin } from "../../apis/auth";
 import { YOUR_FACEBOOK_APP_ID } from "../../config/env";
-import axios from "../../config/axios";
+
 function FacebookLoginButton() {
 	const signUserIn = async response => {
 		console.log("Res -->", response);
-		const { name, email, accessToken, userID } = response;
-		const user = { name, email, accessToken, userId: userID };
-
-		await axios({
-			method: "post",
-			url: "http://localhost:4000/signin/facebook",
-			data: {
-				user,
+		const {
+			name,
+			email,
+			accessToken,
+			userID,
+			picture: {
+				data: { url: profilePic },
 			},
-		});
+		} = response;
+		console.log(profilePic);
+		const user = { name, email, accessToken, profilePic, userId: userID };
+		await facebookLogin(user);
 	};
 
 	return (
 		<FacebookLogin
 			appId={YOUR_FACEBOOK_APP_ID}
-			fields="name,email"
-			scope="public_profile, email"
+			fields="name,email,picture"
+			scope="public_profile, email,user_birthday"
 			callback={signUserIn}
 		/>
 	);
