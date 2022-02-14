f3c918ebf904d9bb6b77fb5573f8b3ae21cc4b9d
Moopz-Dev <pongzthor@gmail.com>
2022-02-14 10:23:58 +0700

    added frontend endpoint for register

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,5 +1,21 @@
 import axios from "../config/axios";
 
 export const facebookLogin = async user => {
-	return await axios.post("/facebooklogin", user);
+	return await axios.post("/auth/facebooklogin", user);
+};
+
+export const registerNewUser = async (
+	username,
+	email,
+	phoneNumber,
+	password,
+	confirmPassword
+) => {
+	return await axios.post("/auth/register", {
+		username,
+		email,
+		phoneNumber,
+		password,
+		confirmPassword,
+	});
 };
