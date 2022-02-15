b4586e9788a98845fd4c6265c113cae7f66da880
Moopz-Dev <pongzthor@gmail.com>
2022-02-15 10:13:58 +0700

    exclude /api from pathing

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8765/api";
+const API_URL = "http://localhost:8765/";
 const YOUR_FACEBOOK_APP_ID = "3085130908415322";
 
 export { API_URL, YOUR_FACEBOOK_APP_ID };
