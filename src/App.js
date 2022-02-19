a934e9e2afb34f3fb77b1682e39d8416e537c26f
Moopz-Dev <pongzthor@gmail.com>
2022-02-19 23:25:44 +0700

    fix minor bugs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Home from "./pages/home/Home.js";
+import Home from "./pages/Home/Home.js";
 import ReelsPage from "./pages/reels/ReelsPage/ReelsPage";
 import Messenger from "./pages/messenger/Messenger.jsx";
 import Login from "./pages/login/LoginPage.js";
 import Register from "./pages/register/Register.js";
-import Comment from "./pages/comment/Comment.js";
+import Comment from "./pages/Comment/Comment.js";
 import UserProfile from "./pages/user/UserProfile.js";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import ModalStory from "./components/utils/ModalStory.js";
@@ -19,25 +19,25 @@ import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const { user } = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
 
-  console.log(user)
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={user ? <Home /> : <Login />} />
-        <Route path="/login" element={user ? <Home /> : <Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reels" element={<ReelsPage />} />
-        <Route path="/comment" element={<Comment />} />
-        <Route path="/user" element={<UserProfile />} />
-        <Route path="/mesenger" element={<Messenger />} />
-        <Route path="/test" element={<ModalStory />} />
-        <Route path="/story" element={<Stories />} />
-        <Route path="/userunknown" element={<UserUnkown />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	console.log(user);
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={user ? <Home /> : <Login />} />
+				<Route path="/login" element={user ? <Home /> : <Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/reels" element={<ReelsPage />} />
+				<Route path="/comment" element={<Comment />} />
+				<Route path="/user" element={<UserProfile />} />
+				<Route path="/mesenger" element={<Messenger />} />
+				<Route path="/test" element={<ModalStory />} />
+				<Route path="/story" element={<Stories />} />
+				<Route path="/userunknown" element={<UserUnkown />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
