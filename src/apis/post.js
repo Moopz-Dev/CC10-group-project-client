c78d31a60d461dbc274513202a900ed22ce668ed
KittitachNil <blackhawkdown_208@hotmail.com>
2022-02-21 11:49:29 +0700

    remove updatePostMedia in post api because don't have it

--- src/apis/post.js
+++ src/apis/post.js
@@ -1,25 +1,21 @@
 import axios from '../config/axios';
 
 export const getAllPosts = async () => {
-  return await axios.get('/posts/all');
+    return await axios.get('/posts/all');
 };
 
 export const getUserPosts = async (userId) => {
-  return await axios.get('/posts/user/' + userId);
+    return await axios.get('/posts/user/' + userId);
 };
 
 export const createPost = async (message, media) => {
-  return await axios.post('/posts/', { message, media });
+    return await axios.post('/posts/', { message, media });
 };
 
 export const updatePost = async (id, message) => {
-  return await axios.patch('/posts/' + id, { message });
-};
-
-export const updatePostMedia = async (id, media) => {
-  return await axios.patch('/posts/' + id, { media });
+    return await axios.patch('/posts/' + id, { message });
 };
 
 export const deletePost = async (id) => {
-  return await axios.delete('/posts/' + id);
+    return await axios.delete('/posts/' + id);
 };
