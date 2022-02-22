1d2000320f1dce64d2f46451be065130b73ced18
Moopz-Dev <pongzthor@gmail.com>
2022-02-22 16:18:27 +0700

    modify createReel

--- src/apis/reel.js
+++ src/apis/reel.js
@@ -1,20 +1,23 @@
-import axios from '../config/axios';
+import axios from "../config/axios";
 
 export const getAllReels = async () => {
-  return await axios.get('/reels/all');
+	return await axios.get("/reels/all");
 };
 
-export const getUserReels = async (userId) => {
-  return await axios.get('/reels/user/' + userId);
+export const getUserReels = async userId => {
+	return await axios.get("/reels/user/" + userId);
 };
 
-export const createReel = async (message) => {
-  return await axios.post('/reels/', { message });
+// export const createReel = async (message) => {
+//   return await axios.post('/reels/', { message });
+// };
+export const createReel = async (message, song, media) => {
+	return await axios.post("/reels/", { message, song, media });
 };
 
 export const updateReel = async (id, message) => {
-  return await axios.patch('/reels/' + id, { message });
+	return await axios.patch("/reels/" + id, { message });
 };
-export const deleteReel = async (id) => {
-  return await axios.delete('/reels/' + id);
+export const deleteReel = async id => {
+	return await axios.delete("/reels/" + id);
 };
