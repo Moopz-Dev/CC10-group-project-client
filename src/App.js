f9f4495d02d5a57fd18a422de0014bd1f4d03b1f
Moopz-Dev <pongzthor@gmail.com>
2022-03-03 12:01:18 +0700

    editted app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Home from "./pages/Home/Home.js";
+import Home from "./pages/home/Home.js";
 import ReelsPage from "./pages/reels/ReelsPage/ReelsPage";
 import Messenger from "./pages/messenger/Messenger.jsx";
-import Login from "./pages/Login/LoginPage.js";
-import Register from "./pages/Register/Register.js";
+import Login from "./pages/login/LoginPage.js";
+import Register from "./pages/register/Register.js";
 import Comment from "./pages/Comment/Comment.js";
 import UserProfile from "./pages/user/UserProfile.js";
 import Stories from "./components/utils/Stories.js";
@@ -17,36 +17,36 @@ import PostReels from "./pages/PostReels/PostReels.js";
 import SearchFriend from "./pages/search/SearchFriend.js";
 
 function App() {
-  const { user } = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
 
-  return (
-    <BrowserRouter>
-      {getToken() ? (
-        !user ? (
-          <Loading />
-        ) : (
-          <Routes>
-            <Route path="/reels" element={<ReelsPage />} />
-            <Route path="/comment" element={<Comment />} />
-            <Route path="/searchfriend" element={<SearchFriend />} />
-            <Route path="/user" element={<UserProfile />} />
-            <Route path="/mesenger" element={<Messenger />} />
-            <Route path="/test" element={<ModalStory />} />
-            <Route path="/story" element={<Stories />} />
-            <Route path="/userunknown" element={<UserUnkown />} />
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        )
-      ) : (
-        <Routes>
-          <Route path="/login" element={user ? <Home /> : <Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      )}
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			{getToken() ? (
+				!user ? (
+					<Loading />
+				) : (
+					<Routes>
+						<Route path="/reels" element={<ReelsPage />} />
+						<Route path="/comment" element={<Comment />} />
+						<Route path="/searchfriend" element={<SearchFriend />} />
+						<Route path="/user" element={<UserProfile />} />
+						<Route path="/mesenger" element={<Messenger />} />
+						<Route path="/test" element={<ModalStory />} />
+						<Route path="/story" element={<Stories />} />
+						<Route path="/userunknown" element={<UserUnkown />} />
+						<Route path="/" element={<Home />} />
+						<Route path="*" element={<Navigate to="/" />} />
+					</Routes>
+				)
+			) : (
+				<Routes>
+					<Route path="/login" element={user ? <Home /> : <Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="*" element={<Navigate to="/login" />} />
+				</Routes>
+			)}
+		</BrowserRouter>
+	);
 }
 
 export default App;
